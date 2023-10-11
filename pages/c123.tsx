821c825f49c8a87f3f8c0645f1c32081d83d43f1
Bayar Ganchimeg <bayar@itrip.mn>
2023-10-11 12:31:30 +0800

    test

--- pages/c123.tsx
+++ pages/c123.tsx
@@ -3,7 +3,7 @@ import '../app/globals.css';
 const c123 = () => {
   return (
     <div className="flex h-[200vh] w-full justify-center bg-green-500 pt-[100px]">
-      <div className="h-[100px] w-[100px] bg-white"></div>
+      <div className="h-[100px] w-[100px] bg-white">test</div>
     </div>
   );
 };
